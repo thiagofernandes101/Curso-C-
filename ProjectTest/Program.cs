12e4b46e07389e4e188ebeec1c798615ddb25560
Thiago Fernandes <thiagofernandes101@outlook.com>
2019-02-23 21:20:05 -0300

    Acrescentada uma mensagem Good morning

--- ProjectTest/Program.cs
+++ ProjectTest/Program.cs
@@ -11,6 +11,7 @@ namespace ProjectTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Good morning");
             Console.ReadLine();
         }
     }
